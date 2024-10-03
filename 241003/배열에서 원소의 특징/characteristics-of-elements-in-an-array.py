aaf5016014ce8ff9119324b7cd875cbd110c5771
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-03 13:52:32 +0900

    Title: 배열에서 원소의 특징, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241003/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
+++ 241003/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
@@ -1,4 +1,5 @@
 arr = list(map(int, input().split()))
-arr2 = [i-1 for i in arr if i % 3 == 0]
-if arr2[0] != None:
-    print(arr2[0])
+for i in arr:
+    if i % 3 == 0:
+        print(i - 1)
+        break;
