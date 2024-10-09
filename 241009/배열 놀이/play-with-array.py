ddd42931804415017c084029505813cab0e4c588
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-09 19:50:09 +0900

    Title: 배열 놀이, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241009/배열 놀이/play-with-array.py
@@ -0,0 +1,21 @@
+inputNQ = input().split()
+n = int(inputNQ[0])
+q = int(inputNQ[1])
+
+index = -1
+arrN = list(map(int, input().split()))
+for i in range(q):
+    arrQ = list(map(int, input().split()))
+    if arrQ[0] == 1:
+        print(arrN[arrQ[1] - 1])
+    elif arrQ[0] == 2:
+        for i in range(len(arrN)):
+            if arrN[i] == arrQ[1]:
+                index = i
+                print(index + 1)
+                break
+        if index == -1:
+            print(0)
+    else:
+        for j in range(arrQ[1] - 1, arrQ[2]):
+            print(arrN[j], end=" ")
