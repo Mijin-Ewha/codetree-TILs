71f27be3a5873c173f6c1683f542d2cb14ddfc0c
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-09 20:09:20 +0900

    Title: 연속부분수열일까, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/연속부분수열일까/contiguous-array-or-not.py
+++ 241009/연속부분수열일까/contiguous-array-or-not.py
@@ -9,11 +9,8 @@ state = "Yes"
 
 for i in range(len(a)):
     if a[i] == b[0]:
-        if i + blength < alength:
-            for k in range(0, blength):
-                if a[i + k] != b[k]:
-                    state = "No"
-                    break
-        else:
-            state = "No"
+        for k in range(1, blength):
+            if a[i + k] != b[k]:
+                state = "No"
+                break
 print(state)
