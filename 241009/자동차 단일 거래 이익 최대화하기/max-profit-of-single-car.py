ccff973cf91eb83d9c3f29783d6b1d2088776230
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-09 21:44:46 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241009/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+temp = sorted(arr)
+
+minIndex = arr.index(temp[0])
+maxVal = arr[minIndex + 1]
+state = 0
+for i in range(minIndex, len(arr)):
+    if minIndex == len(arr):
+        result = 0
+        state = 1
+        break
+    else:
+        if maxVal < arr[i]:
+            maxVal = arr[i]
+
+if state == 1:
+    print(result)
+else:
+    result = maxVal - temp[0]
+    print(result)
