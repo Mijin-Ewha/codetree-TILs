0cf3b6b6648333a3c7311426c05d47a554e6dd88
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-10 18:23:06 +0900

    Title: 문자열 계속 지우기, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 241010/문자열 계속 지우기/keep-removing-string.py
+++ 241010/문자열 계속 지우기/keep-removing-string.py
@@ -4,12 +4,13 @@ b = input()
 while a.count(b) != 0:
     temp = ''
     index = a.find(b)
+    tmIndex = index
     if index == -1:
         break
     for i in range(len(a)):
-        if i == index:
-            for j in range(len(b)):
-                index += j
+        if i == tmIndex:
+            if tmIndex < index + len(b) - 1:
+                tmIndex += 1
         else:
             temp += a[i]
     a = temp
