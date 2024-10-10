250ea62c5ffcf863e86a9601ba1792d541c2aa25
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-10 15:59:44 +0900

    Title: 문자열 전부 붙이기, Time: 114ms, Memory: 27MB, Status: Passed - CodeTree

--- 241010/문자열 전부 붙이기/paste-all-string.py
+++ 241010/문자열 전부 붙이기/paste-all-string.py
@@ -1,8 +1,10 @@
-n = int(input())
+# n = int(input())
 
-total = ''
+# total = ''
 
-for _ in range(n):
-    total += input()
+# for _ in range(n):
+#     total += input()
 
-print(total)
+# print(total)
+
+print(''.join([input() for _ in range(int(input()))]))
