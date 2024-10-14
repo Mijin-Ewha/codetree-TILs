36710cc9fa0289facd3314c45b8abb2912d433a7
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-15 00:38:41 +0900

    Title: 함수를 이용한 윤년 판별, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241015/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,10 @@
+def leapYear(n):
+    if n % 100 == 0 and n % 400 != 0:
+        return "false"
+    elif n % 4 == 0:
+        return "true"
+    else:
+        return "false"
+
+n = int(input())
+print(leapYear(n))
