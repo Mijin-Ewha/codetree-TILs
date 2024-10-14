f3cb6dc580699ebb23fdc477c572f948dccf6f89
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-15 01:56:10 +0900

    Title: 그 계절, 그 날, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241015/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,38 @@
+def leap_year(y):
+    if y % 4 == 0:
+        if y % 100 == 0:
+            if y % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
+def day_exist(y, m, d):
+    if m > 12:
+        return False
+    if (leap_year(y) and m == 2 and d > 28) or (leap_year(y) and m == 2 and d > 29):
+        return False
+    if d > 31:
+        return False
+    elif (m == 2 or m == 4 or m == 6 or m == 9 or m == 11) and d > 30:
+        return False
+    return True
+
+def what_Season(y, m, d):
+    if day_exist(y, m, d):
+        if m >=3 and m <= 5:
+            return "Spring"
+        elif m >=6 and m <= 8:
+            return "Summer"
+        elif m >=9 and m <= 11:
+            return "Fall"
+        elif m == 12 or (m >= 1 and m <= 2):
+            return "Winter"
+    else:
+        return "-1"
+
+arr = list(map(int, input().split()))
+print(what_Season(arr[0], arr[1], arr[2]))
