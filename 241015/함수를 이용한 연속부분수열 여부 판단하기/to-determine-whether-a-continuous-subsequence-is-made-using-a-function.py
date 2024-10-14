99ba6f7344d6d362f6d897205ee45353cfbecdb1
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-15 01:22:49 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241015/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,17 @@
+arr = list(map(int, input().split()))
+n1 = arr[0]
+n2 = arr[1]
+
+n1Arr = list(map(int, input().split()))
+n2Arr = list(map(int, input().split()))
+
+def arr_Find(n1Arr, n2Arr, n1, n2):
+    for i in range(n1):
+        for j in range(n2):
+            if n1Arr[i] == n2Arr[j]:
+                for k in range(n2):
+                    if n1Arr[i + k] != n2Arr[j + k]:
+                        return "No"
+                return "Yes"
+
+print(arr_Find(n1Arr, n2Arr, n1, n2))
