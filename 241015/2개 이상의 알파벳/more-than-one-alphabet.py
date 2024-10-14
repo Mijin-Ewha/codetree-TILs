adf089460da794dff6fcc3bfab93f99dc2307593
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-15 02:24:02 +0900

    Title: 2개 이상의 알파벳, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241015/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,11 @@
+def alpha(a):
+    cnt = 0
+    for i in range(len(a)):
+        for j in range(len(a)):
+            if i != j and a[i] != a[j]:
+                cnt += 1
+            break
+    return cnt
+
+arr = input()
+print("Yes" if alpha(arr) >= 2 else "No")
