dfd10a452814a058f1e800c3614ea9a5bb1a9dae
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2024-10-15 01:05:10 +0900

    Title: 사칙연산 함수, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/사칙연산 함수/quadratic-operations-function.py
+++ 241015/사칙연산 함수/quadratic-operations-function.py
@@ -1,12 +1,14 @@
 def cal(a, o, c):
     if o == '+':
-        return a + c
+        print(a, o, c, '=', a + c)
     elif o == '-':
-        return a - c
+        print(a, o, c, '=', a - c)
     elif o == '/':
-        return int(a / c)
+        print(a, o, c, '=', int(a / c))
+    elif o == '*':
+        print(a, o, c, '=', a * c)
     else:
-        return a * c
+        print("False")
 
 arr = input().split()
-print(int(arr[0]),arr[1], int(arr[2]), '=', cal(int(arr[0]), arr[1], int(arr[2])))
+cal(int(arr[0]), arr[1], int(arr[2]))
